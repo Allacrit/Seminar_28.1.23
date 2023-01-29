fb116ef34175cc22bbd2e4438a801c4ca98b308d
Artem Oleynikov <allacrit@yandex.ru>
2023-01-29 20:36:39 +0300

    Создание решения задачи №4

---
+++ 04/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.WriteLine("4. Написать программу копирования массива.");
+
+int[] initArr = { 0, 1, 2, 3, 4, 5, 6 };
+int[] actualArr = CopyArray(initArr);
+
+Console.WriteLine();
+PrintArray(initArr, preStr: "Исходный массив: ");
+PrintArray(actualArr, preStr: "Скопированный массив: ");
+Console.WriteLine();
+
+
+int[] CopyArray(int[] arr)
+{
+    int[] result = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+        result[i] = arr[i];
+    return result;
+}
+
+void PrintArray(int[] arr, string preStr = "")
+{
+    Console.Write(preStr);
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
